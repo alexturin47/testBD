15ab1e4a2e62cc28ac8b5dac48b47675c4c56818
alexturin <alexturin@gmail.com>
2019-02-18 17:10:28 +0700

    промежуточный вариант

--- testBD/Form1.Designer.cs
+++ testBD/Form1.Designer.cs
@@ -28,13 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "1",
+            "asasad",
+            "5",
+            "7",
+            "2"}, -1);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.count_item = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.name_item = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colvo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.arrive = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.sell = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(381, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "SELECT1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(460, 94);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "SELECT2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(460, 139);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "INSERT";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(460, 189);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "UPDATE";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(460, 241);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "DELETE";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.count_item,
+            this.name_item,
+            this.colvo,
+            this.arrive,
+            this.sell});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1});
+            this.listView1.Location = new System.Drawing.Point(13, 66);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(381, 198);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // count_item
+            // 
+            this.count_item.Text = "№";
+            this.count_item.Width = 30;
+            // 
+            // name_item
+            // 
+            this.name_item.Text = "Наименование";
+            this.name_item.Width = 165;
+            // 
+            // colvo
+            // 
+            this.colvo.Text = "В наличии";
+            // 
+            // arrive
+            // 
+            this.arrive.Text = "Поступило";
+            this.arrive.Width = 64;
+            // 
+            // sell
+            // 
+            this.sell.Text = "Продано";
+            this.sell.Width = 49;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(592, 343);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader count_item;
+        private System.Windows.Forms.ColumnHeader name_item;
+        private System.Windows.Forms.ColumnHeader colvo;
+        private System.Windows.Forms.ColumnHeader arrive;
+        private System.Windows.Forms.ColumnHeader sell;
     }
 }
 
--- testBD/Form1.cs
+++ testBD/Form1.cs
@@ -6,14 +6,84 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace testBD
 {
     public partial class Form1 : Form
     {
+        // строка подключения к MS Access
+        // вариант 1
+        public static string connectString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=test.mdb;";
+        // вариант 2
+        //public static string connectString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=Workers.mdb;";
+
+        private OleDbConnection myConnect;
         public Form1()
         {
             InitializeComponent();
+            myConnect = new OleDbConnection(connectString);
+            myConnect.Open();
+        }
+
+        private void refreshView()
+        {
+            string query = "SELECT name_med, counter, arrival, sell FROM medic ORDER BY name_med";
+            OleDbCommand command = new OleDbCommand(query, myConnect);
+            OleDbDataReader reader = command.ExecuteReader();
+
+            listView1.Items.Clear();
+            int i = 1;
+            while (reader.Read())
+            {
+                ListViewItem item =
+                    listView1.Items.Add(i.ToString());
+                item.SubItems.Add(reader["name_med"].ToString());
+                item.SubItems.Add(reader["counter"].ToString());
+                item.SubItems.Add(reader["arrival"].ToString());
+                item.SubItems.Add(reader["sell"].ToString());
+                i++;
+            }
+
+            reader.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            refreshView();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            myConnect.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "SELECT name_med FROM medic WHERE ID=1";
+
+            OleDbCommand command = new OleDbCommand(query, myConnect);
+            textBox1.Text = command.ExecuteScalar().ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            refreshView();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string insert = "INSERT INTO medic ([name_med], [counter], [arrival], [sell]) Values ('Баралгин', 10, 5, 15)";
+
+            OleDbCommand command = new OleDbCommand(insert, myConnect);
+            command.ExecuteNonQuery();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string query = "UPDATE medic SET [arrival] = 3, [counter] = 8 WHERE [name_med] = 'Анальгин'";
+            OleDbCommand command = new OleDbCommand(query, myConnect);
+            command.ExecuteNonQuery();
         }
     }
 }
